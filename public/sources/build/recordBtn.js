e95d57a76fe9f4bcc38d61c1aeed440fd9a70c17
Jimjamin <jolsenofficial@gmail.com>
2020-09-04 22:04:52 +1000

    [MCO v2.4.6] Clearing up loose ends with offline functionality

--- public/sources/build/recordBtn.js
+++ public/sources/build/recordBtn.js
@@ -3,25 +3,15 @@
 
 import { alertMessage } from './alert.js';
 
-const createGradeForm = (offlineGradeToSave, offlineCommentToSave) => {
+const saveGrades = () => {
+    const url = `${window.location.protocol}//${window.location.host}/grades`;
     let gradesToSave = new FormData();
-    if (offlineGradeToSave) {
-        gradesToSave.append("grade", offlineGradeToSave);
-        gradesToSave.append("comment", offlineCommentToSave);
-    } else {
-        gradesToSave.append("grade", document.getElementById("gradeInput").value);
-        gradesToSave.append("comment", document.getElementById("commentInput").value);
-    }
+    gradesToSave.append("grade", document.getElementById("gradeInput").value);
+    gradesToSave.append("comment", document.getElementById("commentInput").value);
     gradesToSave.append("questionNumber", window.sessionStorage.getItem("questionNumber"));
     gradesToSave.append("studentID", window.sessionStorage.getItem("studentID"));
     gradesToSave.append("examID", window.sessionStorage.getItem("examID"));
     gradesToSave.append("teacherEmail", window.sessionStorage.getItem("teacherEmail"));
-    return gradesToSave;
-}
-
-const saveGrades = (offlineGradeToSave = "", offlineCommentToSave = "") => {
-    const url = `${window.location.protocol}//${window.location.host}/grades`;
-    let gradesToSave = createGradeForm(offlineGradeToSave, offlineCommentToSave);
     const method = {
         method: "POST",
         body: gradesToSave
@@ -30,11 +20,7 @@ const saveGrades = (offlineGradeToSave = "", offlineCommentToSave = "") => {
         .then(response => response.json())
         .then(response => {
             if (response.message) alertMessage(response.message);
-            else if (response.offline) {
-                alertMessage("You are in offline mode; keep your browser open till you're next at school to ensure grades are saved");
-                window.sessionStorage.setItem("grade", `${document.getElementById("gradeInput").value};;;`);
-                window.sessionStorage.setItem("comment", `${document.getElementById("commentInput").value};;;`);
-            }
+            else alertMessage("You are in offline mode; keep your browser open till you're next at school to ensure grades are saved");
             document.getElementById("gradingForm").style.display = "none";
         })
         .catch(error => alertMessage(error.message))
